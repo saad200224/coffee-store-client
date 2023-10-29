1f7804a0e98dfc93326522f5cc89dd1b5127ca11
saad200224 <saad19021994@outlook.com>
2023-10-29 15:21:20 +0600

    first

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Users = () => {
 
     const loadedUsers = useLoaderData();
+    const [users, setUsers] = useState(loadedUsers);
+
+    const handleDelete = id =>{
+        fetch(`http://localhost:5000/user/${id}`, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data =>{
+            if(data.deletedCount > 0){
+                console.log('deleted successfully')
+                // remove the user from user
+                const remainingUsers = users.filter(user => user._id !== id)
+                setUsers(remainingUsers);
+            }
+        })
+
+    }
 
     return (
         <div>
@@ -16,17 +33,21 @@ const Users = () => {
                             <th></th>
                             <th>Email</th>
                             <th>Created At</th>
+                            <th>Last Logged In</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
                         {/* row 1 */}
                         {
-                            loadedUsers.map(user => <tr key={user._id}>
+                            users.map(user => <tr key={user._id}>
                                 <th>1</th>
                                 <td>{user.email}</td>
                                 <td>{user.createdAt}</td>
-                                <td>Blue</td>
+                                <td></td>
+                                <td>
+                                    <button onClick={()=>handleDelete(user._id)} className='btn'>X</button>
+                                </td>
                             </tr>)
                         }
                     </tbody>
